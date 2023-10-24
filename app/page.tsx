fd6f50ebc332b2f055cbcf1b93ad90e3b4b5119d
Lakshay Kamat <lakshaykamat2048@gmail.com>
2023-10-24 10:29:43 +0530

    background changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,55 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
-import calculateAgeInDecimals from "@/util/getAge"; // Replace with the correct import path
+import calculateAgeInDecimals from "@/util/getAge";
 import trimNumberBeforeDecimal from "@/util/trimDecimal";
-type Props = {
-  number:string | null,
-  decimal:string | null
-}
+import { BiSolidMessageSquareEdit } from "react-icons/bi";
+
+type AgeTextProps = {
+  number: String | null;
+  decimal: String | null;
+};
+
+type AgeProps = {
+  birthdate: String;
+  age: String | null;
+};
+
 export default function Home() {
-  const birthdate = "2004-10-02";
-  const [age, setAge] = useState<string|null>(null);
-  const [ageText, setAgeText] = useState<Props>({number:null,decimal:null});
+  const [age, setAge] = useState<AgeProps>({birthdate:"2004-10-02",age:null});
+  const [ageText, setAgeText] = useState<AgeTextProps>({
+    number: null,
+    decimal: null,
+  });
 
   useEffect(() => {
     const updateAge = () => {
-      const calculatedAge = calculateAgeInDecimals(birthdate).toFixed(8);
-     const output = trimNumberBeforeDecimal(calculatedAge)
-      setAgeText({number:output[0],decimal:output[1]})
-      setAge(calculatedAge);
+      const calculatedAge = calculateAgeInDecimals(age.birthdate.toString()).toFixed(8);
+      const output = trimNumberBeforeDecimal(calculatedAge);
+      setAgeText({ number: output[0], decimal: output[1] });
+      setAge({...age,age:calculatedAge});
     };
 
-    const intervalId = setInterval(updateAge, 1000);
+    const intervalId = setInterval(updateAge, 2000);
 
     return () => {
       clearInterval(intervalId);
     };
   }, []);
 
+  const editDOB = ()=>{
+    const dob = prompt("Date of birth")
+    alert("Comming soon.")
+  }
+
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-black">
       <p className="flex justify-center items-center outline min-h-screen">
-          <span className="text-[10rem]">{ageText.number}<sup>{ageText.decimal && "."+ageText.decimal}</sup></span>
-       
+        <span className="text-white text-[10rem]">
+          {ageText.number}
+          <sup>{ageText.decimal && "." + ageText.decimal}</sup>
+        </span>
       </p>
+      <BiSolidMessageSquareEdit onClick={editDOB} className="absolute right-5 bottom-5 text-4xl text-white"/>
     </main>
   );
 }
-
